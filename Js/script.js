e25c0a9812d439e97c0687ba75e0bfc1ea0a7b14
sebasrar97 <juansebas2597@gmail.com>
2023-03-09 00:27:03 -0500

    Website 2
    Fase 1

---
+++ Js/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Negro Medianoche", "Gris camuflaje", "rojo cósmico"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+}) 
